9e5ed6367d3001f1c2ccdb4feff380532e90c883
Akshata72 <sableakshata880@gmail.com>
2022-03-18 21:58:10 +0530

    Added:UC1-Employee_Is_Present_Or_Absent

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Welcome to Employee Wage Computation");
+﻿int Present = 1;
+Random random = new Random();
+int employee_status = random.Next(0, 2);
+if (employee_status == Present)
+{
+    Console.WriteLine("Employeee is Present");
+
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+
+}
