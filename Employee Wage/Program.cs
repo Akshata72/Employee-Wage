7cf97c137d48c484542024fa85ec4e3653e18769
Akshata72 <sableakshata880@gmail.com>
2022-03-18 22:10:19 +0530

    Added:UC5-Calculating_Wage_For_Month

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -1,32 +1,38 @@
-﻿namespace EmployeeWageUC4
+﻿namespace EmployeeWageUC5
 {
     class program
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_HR_PER_RATE = 20;
-
+        public const int NUM_OF_WORKING_DAY = 20;
 
         static void Main(string[] args)
         {
             int empHr = 0;
             int empwage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int TotalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAY; day++)
             {
-                case IS_PART_TIME:
-                    empHr = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHr = 8;
-                    break;
-                default:
-                    empHr = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHr = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHr = 8;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                empwage = (EMP_HR_PER_RATE * empHr);
+                TotalEmpWage += empwage;
+                Console.WriteLine("Daily employee wage:" + empwage);
             }
-            empwage = (EMP_HR_PER_RATE * empHr);
-            Console.WriteLine("Daily employee wage:" + empwage);
+            Console.WriteLine("Total Emp Wage:" + TotalEmpWage);
         }
 
 
