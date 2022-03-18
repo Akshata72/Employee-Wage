e90be253809ccaec65f6f5a06511c66a0736ea50
Akshata72 <sableakshata880@gmail.com>
2022-03-18 22:04:33 +0530

    Added:UC3-Add_PartTime_Employee

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -1,13 +1,27 @@
 ﻿int Present = 1;
+int FullTime = 1;
 int Emp_per_rate = 20;
 int emphr = 0;
 int empwage = 0;
+
 Random random = new Random();
+Random random1 = new Random();
 int employee_status = random.Next(0, 2);
+int EmployeeTime = random.Next(0, 2);
+
 if (employee_status == Present)
 {
-    Console.WriteLine("Employeee is Present");
-    emphr = 8;
+    if (EmployeeTime == FullTime)
+    {
+        Console.WriteLine("Employeee is Present for FullTime");
+        emphr = 8;
+    }
+    else
+    {
+        Console.WriteLine("Employee is Present for PartTime");
+        emphr = 4;
+
+    }
 }
 else
 {
