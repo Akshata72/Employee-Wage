229f04f70e6492b88b5884c920b4ad9a6af2b5d4
Akshata72 <sableakshata880@gmail.com>
2022-03-18 22:02:16 +0530

    Added:UC2-Calculate_Daily_Employee_Wage

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -1,13 +1,18 @@
 ﻿int Present = 1;
+int Emp_per_rate = 20;
+int emphr = 0;
+int empwage = 0;
 Random random = new Random();
 int employee_status = random.Next(0, 2);
 if (employee_status == Present)
 {
     Console.WriteLine("Employeee is Present");
-
+    emphr = 8;
 }
 else
 {
     Console.WriteLine("Employee is Absent");
-
+    emphr = 0;
 }
+empwage = (Emp_per_rate * emphr);
+Console.WriteLine("Daily wage" + empwage);
