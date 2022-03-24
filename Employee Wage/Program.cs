6cb19d32a64147026019c046df6354c3b9b97ee2
Akshata72 <sableakshata880@gmail.com>
2022-03-24 16:23:32 +0530

    Added:UC9-Totalwage_For_Each_Company

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -1,4 +1,4 @@
-﻿class EmpwageBuilderObject
+﻿class CompanyEmpWage
 {
     float EmpRatePerHour = 20;
     public int FullTime_WorkingHrs_PerDay = 8;
@@ -11,7 +11,7 @@
     float EmpDailyWage = 0;
     public int EmpWagePerHour = 0;
     public string Company;
-    public EmpwageBuilderObject(string Company,int EmpRatePerHour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHrs_PerDay, int MAX_WORKING_HR, int MAX_WORKING_DAY)
+    public CompanyEmpWage(string Company,int EmpRatePerHour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHrs_PerDay, int MAX_WORKING_HR, int MAX_WORKING_DAY)
     {
         this.Company = Company;
         this.EmpRatePerHour = EmpRatePerHour;
@@ -20,33 +20,20 @@
         this.MAX_WORKING_HR = MAX_WORKING_HR;
         this.MAX_WORKING_DAY = MAX_WORKING_DAY;
     }
-
-
-
-
     private int IsEmployeePresent()
     {
         Random random = new Random();
         int empcheck = random.Next(0, 3);
         return empcheck;
-       
-
-
-
     }
-
-
-
     public void CalculateWage(string Company)
     {
-         
         float TotalWage = 0;
         int DayNumber = 1;
         int EmpWorkingHrs = 0;
         int TotalWorkingHrs = 0;
         while (DayNumber < MAX_WORKING_DAY && TotalWorkingHrs < MAX_WORKING_HR)
         {
-
             switch (IsEmployeePresent())
             {
                 case IS_PART_TIME:
@@ -63,20 +50,16 @@
             TotalWage += EmpDailyWage;
             DayNumber++;
             TotalWorkingHrs += EmpWorkingHrs;
-           
-
         }
         Console.WriteLine("TotalWorkingHrs working days :" + (DayNumber ) + "\nTotal working hours:" + TotalWorkingHrs);
         Console.WriteLine("Total Employee Wage For " +Company +" Company = " + TotalWage);
-
     }
-
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to employee wage calculatin");
-        EmpwageBuilderObject dMart = new EmpwageBuilderObject("dMart",20, 8, 4, 100, 20);
+        CompanyEmpWage dMart = new CompanyEmpWage("dMart",20, 8, 4, 100, 20);
         dMart.CalculateWage("dMart");
-        EmpwageBuilderObject TATA = new EmpwageBuilderObject("TATA", 20, 8, 4, 100, 20);
+        CompanyEmpWage TATA = new CompanyEmpWage("TATA", 20, 6, 4, 100, 20);
         TATA.CalculateWage("TATA");
     }
 }
